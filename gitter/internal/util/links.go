2fcf8d3f56677b93f6e3ed4fc36b287fde813d4d
indy <indy@mac4>
2023-10-27 11:54:50 -0700

    trim

--- gitter/internal/util/links.go
+++ gitter/internal/util/links.go
@@ -8,7 +8,9 @@ import (
 func CreateCsvLinks(base string, csvLinks string) string {
 	newLinks := make([]string, 0, 5)
 	items := strings.Split(csvLinks, ",")
+	base = strings.TrimRight(base, "/")
 	for _, item := range items {
+		item = strings.TrimLeft(item, "/")
 		newLinks = append(newLinks, fmt.Sprintf("%s/%s", base, item))
 	}
 	return strings.Join(newLinks, " ")
